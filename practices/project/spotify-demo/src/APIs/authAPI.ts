8571150de4fb904e893de698691670e60c4c7263
jaydenkim2001 <92327600+jaydenkim2001@users.noreply.github.com>
2025-11-29 00:53:38 +0000

    29 Nov 2025: Started working on loggin in using the provided authorisation code with PKCE flow from official Spotify documentation.

--- practices/project/spotify-demo/src/APIs/authAPI.ts
+++ practices/project/spotify-demo/src/APIs/authAPI.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { clientID, clientSecret } from "../configs/authConfig";
+import { CLIENT_ID, CLIENT_SECRET } from "../configs/authConfig";
 import { ClientCredentialTokenResponse } from "../typeSpecification/auth";
 
 const encodedBase64 = (data: string): string => {
-  if(typeof window !== "undefined"){
+  if (typeof window !== "undefined") {
     // browser environment
     return btoa(data);
   } else {
@@ -15,9 +15,6 @@ const encodedBase64 = (data: string): string => {
 export const getClientCredentialToken =
   async (): Promise<ClientCredentialTokenResponse> => {
 
-    const tempClientID = "4af1a8408a064d739f86435e2172ca1f";
-    const tempClientSecret = "736fd451f2fb4ab7a9178e72881d601f";
-
     try {
       const body = new URLSearchParams({
         grant_type: "client_credentials",
@@ -28,7 +25,7 @@ export const getClientCredentialToken =
         {
           headers: {
             Authorization: `Basic ${encodedBase64(
-              clientID + ":" + clientSecret
+              CLIENT_ID + ":" + CLIENT_SECRET
             )}`,
             "Content-Type": "application/x-www-form-urlencoded",
           },
--- practices/project/spotify-demo/src/common/components/LoginButton.tsx
+++ practices/project/spotify-demo/src/common/components/LoginButton.tsx
@@ -1,9 +1,14 @@
 import { Button } from '@mui/material'
 import React from 'react'
+import { getSpotifyAuthURL } from '../../utils/auth'
+
+const login = () => {
+  getSpotifyAuthURL()
+}
 
 const LoginButton = () => {
   return (
-    <Button variant='contained' color='secondary' size='large'>
+    <Button variant='contained' color='secondary' size='large' onClick={login}>
         Log in
     </Button>
   )
