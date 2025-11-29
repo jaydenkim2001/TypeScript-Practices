ab18fa3da1f641dcb617fd70419003f069371b1e
jaydenkim2001 <92327600+jaydenkim2001@users.noreply.github.com>
2025-11-29 11:17:30 +0000

    29 Nov 2025: Redirect URI after logging in needs to be resolved.

--- practices/project/spotify-demo/src/APIs/albumAPI.ts
+++ practices/project/spotify-demo/src/APIs/albumAPI.ts
@@ -1,21 +1,20 @@
 import axios from "axios";
-import {  spotifyBaseURL } from "../configs/commonConfig";
+import { SPOTIFY_BASE_URL } from "../configs/commonConfig";
 import { getNewReleasesResponse } from "../typeSpecification/album";
 
 export const getNewReleases = async (
   clientCredentialToken: string
 ): Promise<getNewReleasesResponse> => {
-
   try {
     const response = await axios.get(
-      `${spotifyBaseURL}/browse/new-releases?limit=6`,
+      `${SPOTIFY_BASE_URL}/browse/new-releases?limit=6`,
       {
         headers: {
           Authorization: `Bearer ${clientCredentialToken}`,
         },
       }
     );
-    console.log("URL: " + spotifyBaseURL)
+    console.log("URL: " + SPOTIFY_BASE_URL);
     return response.data;
   } catch (error) {
     throw new Error("Failed to fetch new releases!");
--- practices/project/spotify-demo/src/configs/commonConfig.ts
+++ practices/project/spotify-demo/src/configs/commonConfig.ts
@@ -1,2 +1,2 @@
-export const spotifyBaseURL = process.env.REACT_APP_SPOTIFY_BASE_URL;
-export const REDIRECT_URI = process.env.REACT_APP_REDIRET_URI;
+export const SPOTIFY_BASE_URL = process.env.REACT_APP_SPOTIFY_BASE_URL;
+export const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
